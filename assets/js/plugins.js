b3efbe1fa2f2002b5a7e91259b88f8f66bcd60b6
Larry Geams <larry.geams@gmail.com>
2015-07-16 04:57:22 -1100

    Added carousel script under showcase section

--- assets/js/plugins.js
+++ assets/js/plugins.js
@@ -33,16 +33,19 @@ var logoCarousel = function(){
 	    loop				: true,
 	    margin				: 10,
 	    nav					: false,
-	    autoplay			:true,
-	    autoplayTimeout		:1000,
-	    autoplayHoverPause	:true,
 	    responsive:{
 	        0:{
-	            items:1
+	            items:1,
+	            autoplay			:true,
+			    autoplayTimeout		:1000,
+			    autoplayHoverPause	:true,
 	        },
 	        768:{
 	            items:3,
-	            dots : false
+	            dots : false,
+	            autoplay			:true,
+			    autoplayTimeout		:1000,
+			    autoplayHoverPause	:true,
 	        },
 	        992:{
 	            items: 6,
